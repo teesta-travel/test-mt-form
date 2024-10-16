fc273f7b14e5a24b56f1b4486bd2376bfb3df580
Tanush Gupta <tanushgupta.41@gmail.com>
2024-10-16 20:04:46 +0530

    configured firebase

--- src/app/FirebaseConfig.js
+++ src/app/FirebaseConfig.js
@@ -1,20 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD7EnGk1Rt57-VJGSlsbunIBjjRqbRwXYo",
-  authDomain: "connectbook-4d882.firebaseapp.com",
-  projectId: "connectbook-4d882",
-  storageBucket: "connectbook-4d882.appspot.com",
-  messagingSenderId: "660629583294",
-  appId: "1:660629583294:web:741f039aba7ba73933b5ef"
+  apiKey: "AIzaSyCbHDjddJIVX9djJItb9VilAc1PhT30Bh8",
+  authDomain: "teesta-4c798.firebaseapp.com",
+  databaseURL:
+    "https://teesta-4c798-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "teesta-4c798",
+  storageBucket: "teesta-4c798.appspot.com",
+  messagingSenderId: "982568787762",
+  appId: "1:982568787762:web:c945868b3184b2869f812b",
+  measurementId: "G-ZX0MM1BD7D",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export default db;
+// const analytics = getAnalytics(app);
